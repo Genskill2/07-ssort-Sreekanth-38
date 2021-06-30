e7fa1fc383bbbeeffe3c79da2ca1254c336c7425
Sreekanth-38 <84661591+Sreekanth-38@users.noreply.github.com>
2021-06-30 22:51:55 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,36 +1,23 @@
-#include<stdio.h>
-#include<string.h>
-#include<cs50.h>
-#include<ctype.h>
-#include<math.h>
 
-string ari(string s){
-	int len,num_word=0,num_char=0,num_sent=0;
-	string grade;
-	len = strlen(s);
-	//Find the number of characters, words and sentences.
-	for(int i=0;i<len;i++){
-	  if(isalnum(s[i]))
-	    num_char++;
-		
-	  if(s[i]==' ')
-	    num_word++;
-	  if(s[i]=='!'||s[i]=='?'||s[i]=='.')
-	    num_sent++;
-	    }
-	//ARI value formula
-	float ari_value = 4.71*num_char/num_word+0.5*num_word/num_sent-21.43;
-	//Round Resultant score to next highest integer
-	int r=round(ari_value);
-	if(r<ari_value)
-	  r++;
-	//Return the matching string
-	string grade_level[] = {"KinderKindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor"} ;
-	
-	for(int i =1;i<=14;i++){
-	 if(r==i)
-	   grade = grade_level[i-1];
-		
-	}
-	return grade;
-	}
+#include<stdio.h>
+#include<cs50.h>
+int swap_max(int arr[], int l, int n){
+    int max=arr[n];
+    int pos=n;
+    for(int i=n;i<l;i++){
+      if(arr[i]>max){
+        max=arr[i];
+        pos=i;}}
+    arr[pos]=arr[n];
+    arr[n]=max;
+    
+}
+int ssort(int arr[], int l){
+    for(int i=0; i<l;i++){
+    swap_max(arr, l, i);}
+    printf("the sorted array is \n|");
+    for(int i=0; i<9;i++)
+    printf(" %i |",arr[i]);
+    printf("\n");
+    
+}
